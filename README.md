3df263393cfd8680e2d3dc55e642969050d4190c
jonfalcon <darkutubuki143@gmail.com>
2013-03-26 10:41:22 -0700

    This is my first commit :)

---
+++ README.md
@@ -0,0 +1,82 @@
+<h2>Wordpress TinyMCE Buttons</h2>
+
+<p>Create TinyMCE buttons and button Groups faster with this library. Simple use the single function <code><?php add_tinymce_button($id = "button_id", $settings = array()); ?></code> or create your own class.</p>
+
+<h3>Installation</h3>
+
+<ol>
+	<li>Include the class in your <code>functions.php</code> (for theme development) or in your loader (for plugin development).</li>
+	<li>Add the following code: <code><?php add_tinymce_button($id = "button_id", $settings = array()); ?></code></li>
+	<li>To create button groups, simply add another tinymce button with the same <code>$id</code> of the parent.</li>
+</ol>
+
+<h3>Settings</h3>
+
+<ul>
+	<li><code>title</code> - Button Title</li>
+	<li><code>icon</code> - Button Icon URL</li>
+	<li><code>row</code> - Button row placement. (Up to the 4th row)</li>
+	<li><code>popup</code> - Set this to true if you want to display some settings inside a popup.</li>
+	<li><code>return</code> - This is the string returned to the editor when the button is clicked. If popup is set to true, this will return the content of the popup.</li>
+	<li><code>callback</code> - If you want a function to handle the return string.</li>
+</ul>
+
+<h3>Creating a TinyMCE Button Class</h3>
+
+<p>You can create your own button class if you want to have full control of your button. Here's how you do it</p>
+
+<code>
+	<?php
+		class TinyMCE_Sample_Button implements TinyMCE_Button_Interface {
+			// return id
+			function get_id() {
+				return "my_button_id";
+			}
+
+			// return title
+			function get_title() {
+				return "Sample Button";
+			}
+
+			// return icon url
+			function get_icon() {
+				return '/path/to/icon.ext';
+			}
+
+			// return plugin info
+			function get_info() {
+				return array(
+						"longname"  => "This is a sample Button",
+						"author"    => "Jon Falcon",
+						"authorurl" => "",
+						"version"   => "1.0"
+					);
+			}
+
+			// return placement
+			function get_placement() {
+				return 3;
+			}
+
+			// if this button has popup
+			function has_html() {
+				return true;
+			}
+
+			// popup content
+			function get_return_string() {
+				ob_start();
+				?>
+					<!-- your html goes here -->
+				<?php
+				return ob_get_clean();
+			}
+		}
+		// add your button to the buttons list
+		TinyMCE_Buttons::get_instance()->add_button(new TinyMCE_Sample_Button());
+	?>
+</code>
+
+<h3>TIPS</h3>
+
+<p>If you plan to create a popup, you have to add a javascript to return the string back to the editor. Simple use the function <code>window.send_to_editor("my return string here");</code></p>
---
+++ tinymce-buttons.php
@@ -0,0 +1,368 @@
+<?php
+// Copyright (c) 2013 Jon Falcon
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+/**
+ * TinyMCE Button Interface
+ *
+ * This enables User(s) to create their own TinyMCE Button Class.
+ *
+ * @author   Jon Falcon <darkutubuki143@gmail.com>
+ * @version  1.0
+ */
+interface TinyMCE_Button_Interface {
+	function get_id();
+	function get_title();
+	function get_icon();
+	function get_info();
+	function get_placement();
+	function has_html();
+	function get_return_string();
+}
+
+/**
+ * TinyMCE Button Class
+ *
+ * A class to create a simple tinymce button that returns a string
+ * to the TinyMCE Editor
+ *
+ * @author   Jon Falcon <darkutubuki143@gmail.com>
+ * @version  1.0
+ */
+class TinyMCE_Button implements TinyMCE_Button_Interface {
+	private $id;
+	private $settings;
+
+	/**
+	 * Populates the object if the button id is set
+	 * @param string $id       Button ID
+	 * @param array  $settings Button Settings
+	 */
+	function __construct($id = null, array $settings = array()) {
+		if($id) {
+			$this->populate($id, $settings);
+		}
+	}
+
+	/**
+	 * Populates this object
+	 *
+	 * @param  string $id       Button ID
+	 * @param  array  $settings Button settings
+	 */
+	function populate($id = '', array $settings) {
+		$this->id       = $id;
+
+		$this->settings['title']             = isset($settings['title']) ? $settings['title'] : '';
+		$this->settings['icon']              = isset($settings['icon']) ? $settings['icon'] : '';
+		$this->settings['row']               = isset($settings['row']) ? $settings['row'] : 3;
+		$this->settings['popup']             = isset($settings['popup']) ? $settings['popup'] : false;
+		$this->settings['return']            = isset($settings['return']) ? $settings['return'] : '';
+		$this->settings['callback']          = isset($settings['callback']) ? $settings['callback'] : '';
+		// plugin info
+		$this->settings['info']['longname']  = isset($settings['info']['longname']) ? $settings['info']['longname'] : '';
+		$this->settings['info']['author']    = isset($settings['info']['author']) ? $settings['info']['author'] : '';
+		$this->settings['info']['authorurl'] = isset($settings['info']['authorurl']) ? $settings['info']['authorurl'] : '';
+		$this->settings['info']['version']   = isset($settings['info']['version']) ? $settings['info']['version'] : '';
+	}
+
+	/**
+	 * Returns the button ID
+	 * @return string Button ID
+	 */
+	function get_id() {
+		return $this->id;
+	}
+
+	/**
+	 * Returns all the settings
+	 * @return array Button Settings
+	 */
+	function get_settings() {
+		return $this->settings;
+	}
+
+	/**
+	 * Returns the button title
+	 * @return string Button Title
+	 */
+	function get_title() {
+		return $this->settings['title'];
+	}
+
+	/**
+	 * Returns the button icon
+	 * @return string Button Icon URL
+	 */
+	function get_icon() {
+		return $this->settings['icon'];
+	}
+
+	/**
+	 * Returns the information
+	 * @return array       Button Information
+	 */
+	function get_info() {
+		return $this->settings['info'];
+	}
+
+	/**
+	 * Returns the button row placement
+	 * @return integer Row number
+	 */
+	function get_placement() {
+		return intval($this->settings['row']);
+	}
+
+	/**
+	 * Has html popup content
+	 *
+	 * Always return true since we are only making a simple button
+	 * 
+	 * @return boolean 
+	 */
+	function has_html() {
+		return $this->settings['popup'];
+	}
+
+	/**
+	 * Returns the string to be added on the TinyMCE Editor
+	 * @return string String added to the TinyMCE Editor
+	 */
+	function get_return_string() {
+		if(function_exists($this->settings['callback'])) {
+			return call_user_func($this->settings['callback']);
+		}
+		return $this->settings['return'];
+	}
+}
+
+/**
+ * TinyMCE Buttons Class
+ *
+ * A singleton class that handles all the required WP Hooks and Callbacks
+ * to generate the buttons.
+ */
+class TinyMCE_Buttons {
+	private $buttons = array();
+	static $self;
+
+	/**
+	 * Constructor
+	 *
+	 * Restricts instantiation of this class
+	 * @access  private
+	 */
+	private function __construct() {}
+
+	/**
+	 * Returns the last instance of this class or create a new instance
+	 * if this class hans't been instantiated yet
+	 * @return Object TinyMCE_Buttons Class
+	 */
+	static public function get_instance() {
+		if(!isset(self::$self)) {
+			self::$self = new self();
+		}
+		return self::$self;
+	}
+
+	/**
+	 * Registers the TinyMCE Button or override the existing one
+	 * @param TinyMCE_Button_Interface $button TinyMCE Button
+	 */
+	function add_button(TinyMCE_Button_Interface $button) {
+		$button_id                   = $button->get_id();
+		$this->buttons[$button_id][] = $button;
+	}
+
+	function install() {
+		/*** register buttons ***/
+		add_action('init', array($this, 'initialize_buttons'));
+		add_filter('mce_buttons', array($this, 'buttons_on_row_1'));
+		add_filter('mce_buttons_2', array($this, 'buttons_on_row_2'));
+		add_filter('mce_buttons_3', array($this, 'buttons_on_row_3'));
+		add_filter('mce_buttons_4', array($this, 'buttons_on_row_4'));
+
+		/*** AJAX REQUEST ***/
+		add_action('wp_ajax_tinymce_view_html', array($this, 'view_html'));
+		add_action('wp_ajax_tinymce_button_script', array($this, 'editor_script'));
+	}
+
+	function initialize_buttons() {
+		if($this->_validate_user()) {
+			add_filter('mce_external_plugins', array($this, 'register_script'));
+		}
+	}
+
+	function buttons_on_row_1($buttons) {
+		return $this->_display_buttons($buttons, 1);
+	}
+
+	function buttons_on_row_2($buttons) {
+		return $this->_display_buttons($buttons, 2);
+	}
+
+	function buttons_on_row_3($buttons) {
+		return $this->_display_buttons($buttons, 3);
+	}
+
+	function buttons_on_row_4($buttons) {
+		return $this->_display_buttons($buttons, 4);
+	}
+
+	function register_script($plugins) {
+		foreach($this->buttons as $button_group) {
+			$button_id           = $button_group[0]->get_id();
+			$plugins[$button_id] = admin_url('admin-ajax.php?action=tinymce_button_script&button=' . $button_id);
+		}
+		return $plugins;
+	}
+
+	function view_html() {
+		$plugin = isset($_GET['plugin']) ? $_GET['plugin'] : '';
+		$index  = isset($_GET['index']) ? $_GET['index'] : '';
+		$button = $this->get_button($plugin, $index);
+
+		if($button){
+			$button->get_return_string();
+		}
+		exit(0);
+	}
+
+	function editor_script() {
+		header("Content-type: text/javascript");
+		?>
+			(function(){
+				<?php foreach($this->buttons as $button_group): $parent = $button_group[0]; ?>
+					tinymce.create( "tinymce.plugins.<?php echo $parent->get_id(); ?>", {
+						getInfo		  : function () {
+											return <?php echo $this->_info_to_object($parent->get_info()); ?>;
+										},
+						<?php if(count($button_group) == 1): ?>
+						init		  : function(ed, url){
+											ed.addButton( "<?php echo $parent->get_id(); ?>", {
+												title	: "<?php echo $parent->get_title(); ?>",
+												image	: "<?php echo $parent->get_icon(); ?>",
+												onclick	: function(){
+													<?php if($parent->has_html()): ?>
+														tb_show('<?php echo $parent->get_title(); ?>', '<?php echo admin_url('admin-ajax.php?action=tinymce_view_html&plugin=' . $parent->get_id() . '&index=0'); ?>');
+														console.log('<?php echo admin_url('admin-ajax.php?action=tinymce_view_html&plugin=' . $parent->get_id() . '&index=0'); ?>');
+													<?php else: ?>
+														ed.execCommand( "mceInsertContent", false, '<?php echo $parent->get_return_string(); ?>' );
+													<?php endif; ?>
+												}
+											} );
+										},
+						<?php endif; ?>
+						createControl : function(n, cm) {
+											<?php if(count($button_group) > 1): ?>
+												if(n == '<?php echo $parent->get_id(); ?>') {
+													var c = cm.createSplitButton('<?php echo $parent->get_id(); ?>', {
+									                    title : '<?php echo $parent->get_title(); ?>',
+									                    image	: "<?php echo $parent->get_icon(); ?>",
+									                    onclick : function() {
+										                    <?php if($parent->has_html()): ?>
+										                    	tb_show('<?php echo $parent->get_title(); ?>', '<?php echo admin_url('admin-ajax.php?action=tinymce_view_html&plugin=' . $parent->get_id() . '&index=0'); ?>');
+															<?php else: ?>
+									                    		tinyMCE.activeEditor.execCommand('mceInsertContent', false, '<?php echo $parent->get_return_string(); ?>');
+									                    	<?php endif; ?>
+									                    }
+									                });
+								                    c.onRenderMenu.add(function(c, m) {
+														<?php foreach($button_group as $count => $button): ?>
+															m.add({title : '<?php echo $button->get_title(); ?>', onclick : function() {
+																<?php if($button->has_html()): ?>
+																	tb_show('<?php echo $button->get_title(); ?>', '<?php echo admin_url('admin-ajax.php?action=tinymce_view_html&plugin=' . $button->get_id() . '&index=0'); ?>');
+																<?php else: ?>
+										                        	tinyMCE.activeEditor.execCommand('mceInsertContent', false, '<?php echo $button->get_return_string(); ?>');
+											                    <?php endif; ?>
+										                    }});
+														<?php endforeach; ?>
+									 				});
+									 				return c;
+												}
+											<?php endif; ?>
+											return null;
+										}
+					} );
+					tinymce.PluginManager.add( "<?php echo $parent->get_id(); ?>", tinymce.plugins.<?php echo $parent->get_id(); ?> );
+				<?php endforeach; ?>
+			})();
+		<?php
+		exit(0);
+	}
+
+	function has_button($id) {
+		foreach($this->buttons as $button_group) {
+			foreach($button_group as $button) {
+				if($button->get_id() == $id) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	function get_button($id, $index) {
+		if(isset($this->buttons[$id][$index])) {
+			return $this->buttons[$id][$index];
+		}
+
+		return false;
+	}
+
+	private function _info_to_object($info) {
+		$arr   = array("longname", "author", "authorurl", "version");
+		$infos = array();
+		foreach($arr as $in) {
+			if(isset($info[$in])) {
+				$infos[$in] = $info[$in];
+			} else {
+				$infos[$in] = '';
+			}
+		}
+
+		return json_encode($infos);
+	}
+
+	private function _display_buttons($buttons, $row) {
+		foreach($this->buttons as $button_group) {
+			if($button_group[0]->get_placement() == $row) {
+				array_unshift($buttons, $button_group[0]->get_id());
+			}
+		}
+
+		return $buttons;
+	}
+
+	private function _validate_user() {
+		return (current_user_can("edit_posts") && current_user_can("edit_pages") && !empty($this->buttons));
+	}
+}
+
+/**
+ * Add a new button on the TinyMCE Editor
+ * @param string $id       Button Id
+ * @param array  $settings Settings Array
+ */
+function add_tinymce_button($id, $settings) {
+	TinyMCE_Buttons::get_instance()->add_button(new TinyMCE_Button($id, $settings));
+}
